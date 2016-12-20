d45349962009cd36efd3870647be3c03f98605bc
David Davydov <kctrlv@gmail.com>
2016-12-20 09:54:58 -0700

    client listens for status event

--- public/client.js
+++ public/client.js
@@ -8,9 +8,16 @@ var socket = io();
 
 // ...
 
-// listen for server events and update the html
+// listen for the server event to all users and update the html
 socket.on('usersConnected', pageUpdateCount )
 
 function pageUpdateCount(count) {
   document.getElementById('connection-count').innerText = "Connected Users: " + count;
+}
+
+// listen for the server event to me about my status
+socket.on('statusMessage', pageUpdateStatus)
+
+function pageUpdateStatus(message) {
+  document.getElementById('status-message').innerText = message
 }
