37036cf69b9a0a8e529d82bd8fefe648a9b9203b
David Davydov <kctrlv@gmail.com>
2016-12-20 09:48:58 -0700

    client updates when users connect disconnect

--- public/client.js
+++ public/client.js
@@ -5,3 +5,12 @@ var socket = io();
 // when connecting, a connection event was fired on the server
 // right now, nothing is listening for it, so it's useless
 // next step: set up an event listener on the server for the connection event
+
+// ...
+
+// listen for server events and update the html
+socket.on('usersConnected', pageUpdateCount )
+
+function pageUpdateCount(count) {
+  document.getElementById('connection-count').innerText = "Connected Users: " + count;
+}
