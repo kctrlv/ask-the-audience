ee88ba287fbd0662aa3c9628742d6762bc0c1e4c
David Davydov <kctrlv@gmail.com>
2016-12-20 09:24:19 -0700

    hello world with express

---
+++ server.js
@@ -0,0 +1,30 @@
+// require libraries
+const http = require('http');
+const express = require('express');
+
+// instantiate express
+const app = express();
+
+// serve public directory
+app.use(express.static('public'));
+
+// route root to index.html
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+// app needs to be passed to http module to produce a server
+var server = http.createServer(app);
+
+// pass a port set in env vars || fallback port
+var port = process.env.PORT || 3000;
+
+// run the server
+server.listen(port, function() {
+  console.log('Listening on port ' + port + '.')
+})
+
+// export the server so we can access it - public interface
+// other modules which require this module will be able to access this object and use the functionality
+
+module.exports = server;
